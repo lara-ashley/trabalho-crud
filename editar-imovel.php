00d365c34c4a7a0f9dc1b77fe3bb1a4fa4bafaad
lara-ashley <lara.goncalves14@aluno.ce.gov.br>
2025-06-25 19:25:16 -0300

    editar-imovel css

--- editar-imovel.php
+++ editar-imovel.php
@@ -45,30 +45,83 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 ?>
-<form class="editar-imovel" method="post">
-    <label>Nome do proprietário</label>
-    <input type="text" name="nome" value="<?= htmlspecialchars($anuncio['nome']) ?>">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de editar cadastro do imóvel</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
 
-    <label>Tipo do imóvel</label>
-    <input type="text" name="tipo" value="<?= htmlspecialchars($anuncio['tipo']) ?>">
+</head>
+<body class ="main-editar-imovel">
 
-    <label>Cidade</label>
-    <input type="text" name="cidade" value="<?= htmlspecialchars($anuncio['cidade']) ?>"><br>
+ <section class="navbar">
 
-    <label>Bairro</label>
-    <input type="text" name="bairro" value="<?= htmlspecialchars($anuncio['bairro']) ?>"><br>
+        <a class="link" href="painel.php">Ínicio</a>
+        <a class="link" href="imoveis.php">Compra</a>
+        <a class="link" href="anuncio.php">Anúncio</a>
+        <a class="link" href="busca.php">Buscar imóveis</a>
+        <a class="link" href="conta.php">Conta</a>
+        <img class="menu" src="assets/menu" alt="Menu">
+        
+    </section>
 
-    <label>Número de cômodos</label>
-    <input type="number" name="comodos" value="<?= htmlspecialchars($anuncio['comodos']) ?>"><br>
+    <form class="editar-imovel" method="post">
+        <label>Nome do proprietário</label>
+        <input class = "input-editar-imovel" type="text" name="nome" value="<?= htmlspecialchars($anuncio['nome']) ?>">
 
-    <label>Matrícula do cartório</label>
-    <input type="text" name="matricula" value="<?= htmlspecialchars($anuncio['matricula']) ?>"><br>
+        <label>Tipo do imóvel</label>
+        <input class = "input-editar-imovel" type="text" name="tipo" value="<?= htmlspecialchars($anuncio['tipo']) ?>">
 
-    <label>Metragem do imóvel</label>
-    <input type="number" step="0.01" name="metragem" value="<?= htmlspecialchars($anuncio['metragem']) ?>"><br>
+        <label>Cidade</label>
+        <input class = "input-editar-imovel" type="text" name="cidade" value="<?= htmlspecialchars($anuncio['cidade']) ?>"><br>
 
-    <label>Valor do imóvel</label>
-    <input type="number" step="0.01" name="valor" value="<?= htmlspecialchars($anuncio['valor']) ?>"><br>
+        <label>Bairro</label>
+        <input class = "input-editar-imovel" type="text" name="bairro" value="<?= htmlspecialchars($anuncio['bairro']) ?>"><br>
 
-    <button type="submit">Salvar alterações</button>
-</form>
+        <label>Número de cômodos</label>
+        <input class = "input-editar-imovel" type="number" name="comodos" value="<?= htmlspecialchars($anuncio['comodos']) ?>"><br>
+
+        <label>Matrícula do cartório</label>
+        <input class = "input-editar-imovel" type="text" name="matricula" value="<?= htmlspecialchars($anuncio['matricula']) ?>"><br>
+
+        <label>Metragem do imóvel</label>
+        <input class = "input-editar-imovel" type="number" step="0.01" name="metragem" value="<?= htmlspecialchars($anuncio['metragem']) ?>"><br>
+
+        <label>Valor do imóvel</label>
+        <input class = "input-editar-imovel" type="number" step="0.01" name="valor" value="<?= htmlspecialchars($anuncio['valor']) ?>"><br>
+
+        <button class = "botao-editar-imovel" type="submit">Salvar alterações</button>
+    </form>
+
+    
+    <footer class="footer">
+        <div class="container-footer">
+            <div class="footer-logo">
+                <img src="assets/logo_seular.png" alt="Logo Seu Lar">
+                <p>&copy; <?= date("Y") ?> Seu Lar. Todos os direitos reservados.</p>
+            </div>
+
+            <div class="footer-links">
+                <a href="sobre.php">Sobre Nós</a>
+                <a href="contato.php">Contato</a>
+                <a href="termos.php">Termos de Uso</a>
+                <a href="privacidade.php">Política de Privacidade</a>
+                <a href="logout.php">Sair</a>
+                <a href="tela-cadastro.php">Cadastre-se</a>
+                <a href="index.php">Acessar conta</a>
+            </div>
+
+            <div class="footer-social">
+                <a href="https://facebook.com" target="_blank">Facebook</a>
+                <a href="https://instagram.com" target="_blank">Instagram</a>
+                <a href="https://linkedin.com" target="_blank">LinkedIn</a>
+            </div>
+
+    </footer>
+
+
+</body>
+</html>
