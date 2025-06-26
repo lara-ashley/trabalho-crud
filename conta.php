e94dc04753c70716fc123e7da03d0d249e7efde0
lara-ashley <lara.goncalves14@aluno.ce.gov.br>
2025-06-25 22:56:39 -0300

    novo estilo

--- conta.php
+++ conta.php
@@ -15,7 +15,7 @@ $msg = $_GET['msg'] ?? '';
 <html lang="pt-br">
 
 <head>
-
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de anúncio</title>
